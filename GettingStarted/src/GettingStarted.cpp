a5ec5c460c135f282a4b36199b6d534bdbdbae09
Jose Cisneros <jcisne_007@hotmail.com>
2013-05-10 02:43:18 -0500

    Creating Proyect

---
+++ GettingStarted/src/GettingStarted.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : GettingStarted.cpp
+// Author      : Jose Cisneros
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Geting Started - simple codes" << endl; // prints Geting Started - simple codes
+	return 0;
+}
