6169a319608c11411006abe5980cdb3bd943b97f
kroakker <german@xreal.me>
2013-10-29 23:29:20 +0100

    complete TestVideo

---
+++ TestVideo/obj/local/armeabi-v7a/objs-debug/testvideo/TestVideo.o.d
@@ -0,0 +1,22 @@
+obj/local/armeabi-v7a/objs-debug/testvideo/TestVideo.o: jni/TestVideo.cpp \
+ C:/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/jni.h \
+ C:/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/string.h \
+ C:/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ C:/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/android/api-level.h \
+ C:/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/malloc.h \
+ jni/include/com_example_testvideo_MainActivity.h
+
+C:/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/jni.h:
+
+C:/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/string.h:
+
+C:/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+C:/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/android/api-level.h:
+
+C:/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/malloc.h:
+
+jni/include/com_example_testvideo_MainActivity.h:
